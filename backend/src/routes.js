f8ac36ac0833b32f67c69a55863c0c4c3276b289
Maicon Andraski <maiconm23@gmail.com>
2020-03-25 22:09:59 -0300

    🚹 feat(backend): profile route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,11 +2,15 @@ const express = require('express')
 
 const OngController = require('./controllers/OngControllers')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.index)
+
 routes.get('/incidents', IncidentController.index)
 routes.post('/incidents', IncidentController.create)
 routes.delete('/incidents/:id', IncidentController.delete)
